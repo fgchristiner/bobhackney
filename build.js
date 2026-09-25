525c4d8b91d324a1406ac7a3a2e0396b1100cc37
fgchristiner <cremenih@gmail.com>
2026-09-24 21:45:17 -0700

    Update build.js

--- build.js
+++ build.js
@@ -22,7 +22,7 @@ async function fetchRecords() {
   let records = [];
   let offset;
   do {
-    const url = new URL(`https://api.airtable.com/v0/${BASE_ID}/${encodeURIComponent(TABLE)}`);
+    const url = new URL(`https://api.airtable.com/v0/${patAQbLt3DbLz2lns}/${encodeURIComponent(TABLE)}`);
     url.searchParams.set('pageSize', '100');
     if (offset) url.searchParams.set('offset', offset);
     const res = await fetch(url, { headers: { Authorization: `Bearer ${TOKEN}` } });
